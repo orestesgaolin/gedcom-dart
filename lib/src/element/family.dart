cfaa678b7ceba36d5f392290a97aa576fe98d1e2
Dominik Roszkowski <dominik@roszkowski.dev>
2020-10-19 11:26:00 +0200

    feat: add helper methods to family element

--- lib/src/element/family.dart
+++ lib/src/element/family.dart
@@ -24,6 +24,48 @@ class FamilyElement extends GedcomElement {
   @override
   String get tag => GEDCOM_TAG_FAMILY;
 
+  /// Returns the husband/father of the family
+  ///
+  /// Returns null if not found
+  IndividualElement getHusband(Map<String, GedcomElement> elementsMap) {
+    final husbandId = children.firstWhere(
+        (element) => element.tag == GEDCOM_TAG_HUSBAND,
+        orElse: () => null);
+    if (husbandId != null) {
+      final husband = elementsMap[husbandId.value];
+      return husband;
+    }
+    return null;
+  }
+
+  /// Returns the wife/mother of the family
+  ///
+  /// Returns null if not found
+  IndividualElement getWife(Map<String, GedcomElement> elementsMap) {
+    final wifeId = children.firstWhere(
+        (element) => element.tag == GEDCOM_TAG_WIFE,
+        orElse: () => null);
+    if (wifeId != null) {
+      final wife = elementsMap[wifeId.value];
+      return wife;
+    }
+    return null;
+  }
+
+  /// Returns the children sorted by the birth year if available
+  ///
+  /// Returns empty list if no children found
+  List<IndividualElement> getChildren(Map<String, GedcomElement> elementsMap) {
+    final childrenElements = <IndividualElement>[];
+    final childrenIds =
+        children.where((element) => element.tag == GEDCOM_TAG_CHILD).toList();
+    for (final child in childrenIds) {
+      childrenElements.add(elementsMap[child.value]);
+    }
+    childrenElements.sort();
+    return childrenElements;
+  }
+
   /// Returns copy of the element
   @override
   FamilyElement copyWith({
